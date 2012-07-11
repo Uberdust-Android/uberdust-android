2a4540c3195af9767ee87182de3f91dfb855267b
Vasileios Keramidas <keramidasceid@gmail.com>
2012-07-11 23:56:02 +0300

    trying zxing

--- src/eu/uberdust/mobileclient/FourSquareActivity.java
+++ src/eu/uberdust/mobileclient/FourSquareActivity.java
@@ -21,6 +21,7 @@ import com.google.zxing.integration.android.IntentResult;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * This is the activity for feature 5 in the dashboard application.
@@ -49,7 +50,7 @@ protected void onCreate(Bundle savedInstanceState)
     setContentView (R.layout.activity_foursquare);
     setTitleFromActivityLabel (R.id.title_text);
     
-    IntentIntegrator integrator = new IntentIntegrator(FourSquareActivity.this);
+    IntentIntegrator integrator = new IntentIntegrator(this);
     integrator.initiateScan();
     
 }
@@ -58,8 +59,10 @@ public void onActivityResult(int requestCode, int resultCode, Intent intent) {
 	  IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 	  if (scanResult != null) {
 	    // handle scan result
+		Log.v("MINE", "SUCCESS");
 	  }
 	  // else continue with any other code you need in the method
+	  Log.v("MINE", "FAILURE");
 	}
 
 } // end class
